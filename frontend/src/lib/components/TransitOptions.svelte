7d0d268ddcd5a016532bfc9871c94624f21e34fd
Sorano7 <sorano27@outlook.com>
2025-05-09 10:23:08 -0400

    Refactor API constants

--- frontend/src/lib/components/TransitOptions.svelte
+++ frontend/src/lib/components/TransitOptions.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import { getSystemState, loadData, API_CLOCK } from "./System.svelte";
+  import { getSystemState, loadData } from "$lib/system.svelte";
 
   const systemState = getSystemState();
 
